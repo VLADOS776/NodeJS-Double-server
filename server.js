5133c902991d22332d4e8d693e89b692ac17d606
VLADOS776 <kurtukovvlad@gmail.com>
2016-11-06 15:47:26 +0300

    Update

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
+var WebSocketServer = new require("ws");
+var http            = new require('http');
 var config          = new require("./libs/config");
 var players         = new require("./libs/clients");
 var double          = new require("./libs/double");
-var WebSocketServer = new require("ws");
-
-var clients = {};
-var lastNumbers = [];
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var webSocketServer = new WebSocketServer.Server({
-    port: 8000
+var server = http.createServer(function(req, res) {
+    console.log((new Date())+' request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Welcome to Node.js on OpenShift');
+    res.end('Thanks');
 });
 
-webSocketServer.on('connection', function(ws) {
+server.listen(port);
+
+var wss = new WebSocketServer.Server({
+    server: server
+    //port: 8080
+});
+
+wss.on('connection', function(ws) {
     console.log('newClient');
     players.newClient(ws);
 })
