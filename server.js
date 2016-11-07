b08e39d07ccebd6ff4f5bb5e11d8a1d4a17c85c5
VLADOS776 <kurtukovvlad@gmail.com>
2016-11-07 13:05:07 +0300

    Server test

--- server.js
+++ server.js
@@ -1,30 +1,47 @@
-var WebSocketServer = new require("ws");
+var WebSocketServer = new require("ws").Server;
 var http            = new require('http');
+var express         = new require('express');
 var config          = new require("./libs/config");
 var players         = new require("./libs/clients");
 var double          = new require("./libs/double");
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT;
+var app = express();
 
-var server = http.createServer(function(req, res) {
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+
+app.set('port', port);
+app.set('ipaddress', ipaddress);
+
+var server = http.createServer(/*function(req, res) {
     console.log((new Date())+' request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Welcome to Node.js on OpenShift');
-    res.end('Thanks');
-});
+    res.write('Welcome to Node.js on OpenShift\n\n');
+    res.end('Thanks for visiting');
+}*/app);
 
-server.listen(port, ipaddress, function() {
-    console.log((new Date()) + ' Server is listening on port '+ port);
-});
+var io = require('socket.io').listen(server);
 
-var wss = new WebSocketServer.Server({
+io.sockets.on('connection', function(socket) {
+    socket.emit('news', {hello: 'world'});
+    socket.on('other event', function(data) {
+        console.log(data);
+    })
+})
+
+server.listen(app.get('port'), app.get('ipaddress'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+})
+
+/*
+wss = new WebSocketServer({
     server: server
     //port: 8080
 });
 
+console.log(wss);
+
 wss.on('connection', function(ws) {
-    console.log('newClient');
     players.newClient(ws);
 })
 
@@ -32,3 +49,6 @@ console.log('Listening to '+ ipaddress + ':' + port + '...');
 
 double.newGame();
 
+server.listen(port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port '+ port);
+});*/
